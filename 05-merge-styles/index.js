2e5b6a9122deb30f2f6e44d0cd93bcfa3a4bf72f
HelgaAthame <athamethedarkest@gmail.com>
2022-10-31 23:21:40 +0300

    fix: correct mistakes in 5th-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,10 +2,10 @@ const path = require('path');
 const folder = require('fs/promises');
 const fs = require('fs');
 
-async function bundler () {
-  const files = await folder.readdir(path.join(__dirname, 'styles'), {withFileTypes: true});
+async function bundler() {
+  const files = await folder.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
   let arr = [];
-  for(let file of files){
+  for (let file of files) {
     if (file.isFile()) {
       const fileExtension = file.name.match(/\.[A-Za-z\d]+/)[0].slice(1);
       if (fileExtension == 'css') {
@@ -16,15 +16,15 @@ async function bundler () {
 
     }
   };
-
-  await fs.access((path.join(__dirname, 'project-dist', 'bundle.css')), /*constants.F_OK,*/ (err) => {
+  folder.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), arr.join("\n"));
+  /*await fs.access((path.join(__dirname, 'project-dist', 'bundle.css')), (err) => {
     if (err) {
       folder.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), arr.join("\n"));
     } else {
-      //console.error('myfile already exists');
+      //console.error('my file already exists');
       return;
     }
-  });
+  });*/
 
 }
 
