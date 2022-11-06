33109d30191ba9c4561056c0c9fbdb83079efa09
HelgaAthame <athamethedarkest@gmail.com>
2022-11-06 13:28:51 +0300

    fix: correct build page algorythm

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,12 +2,13 @@ const path = require('path');
 const folder = require('fs/promises');
 
 async function filesInFolder() {
-  const files = await folder.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
-  for(let file of files){
+  const files = await folder.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
+  console.log(files);
+  for (let file of files) {
     if (file.isFile()) {
-      const fileName = await path.extname(path.join(__dirname, file.name)).slice(1);
+      const fileExt = await path.extname(path.join(__dirname, file.name)).slice(1);
       const fileStat = await folder.lstat(path.join(__dirname, 'secret-folder', file.name));
-      console.log(file.name.match(/[A-Za-z]+/)[0] + " - " + fileName + " - " + fileStat.size/1000 + 'kb');
+      console.log(path.parse(file.name).name + " - " + fileExt + " - " + fileStat.size / 1000 + 'kb');
     }
   };
 }
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const folder = require('fs/promises');
 
-async function bundler () {
-  const projectDist = await folder.mkdir(path.join(__dirname, 'project-dist'), {recursive: true});
+async function bundler() {
+  const projectDist = await folder.mkdir(path.join(__dirname, 'project-dist'), { recursive: true });
   let template = await folder.readFile(path.join(__dirname, 'template.html'), { encoding: 'utf8' });
   const componentNames = template.match(/{{[A-Za-z]+}}/g);
-  const components = await folder.readdir(path.join(__dirname, 'components'), {withFileTypes: false});
-  for (let i=0; i<components.length; i++) {
-    for (let j=0; j<componentNames.length; j++)
-    if (componentNames[j].slice(2,-2) == components[i].match(/[A-Za-z]+/)[0]) {
-      const content = await folder.readFile(path.join(__dirname, 'components', components[i]), { encoding: 'utf8' });
-      template = template.replace(componentNames[j], content);
-    }
+  const components = await folder.readdir(path.join(__dirname, 'components'), { withFileTypes: false });
+  for (let i = 0; i < components.length; i++) {
+    for (let j = 0; j < componentNames.length; j++)
+      if (componentNames[j].slice(2, -2) == components[i].match(/[A-Za-z]+/)[0]) {
+        const content = await folder.readFile(path.join(__dirname, 'components', components[i]), { encoding: 'utf8' });
+        template = template.replace(componentNames[j], content);
+      }
   }
   await folder.writeFile(path.join(__dirname, 'project-dist', 'index.html'), template);
 }
 
-async function styler () {
-  const files = await folder.readdir(path.join(__dirname, 'styles'), {withFileTypes: true});
+async function styler() {
+  const files = await folder.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
   let arr = [];
-  for(let file of files){
+  for (let file of files) {
     if (file.isFile()) {
       const fileExtension = file.name.match(/\.[A-Za-z\d]+/)[0].slice(1);
       if (fileExtension == 'css') {
@@ -33,31 +33,46 @@ async function styler () {
   await folder.writeFile(path.join(__dirname, 'project-dist', 'style.css'), arr.join("\n"));
 }
 
-async function copier (folderPath, destPath) {
-  const folderCopy = await folder.mkdir(destPath, {recursive: true});
-  const filesCopied = await folder.readdir(destPath, {withFileTypes: true});
-  /*for(let fileCopied of filesCopied){
-    await folder.unlink(path.join(destPath, fileCopied.name));
-  }*/
-  const files = await folder.readdir(folderPath, {withFileTypes: true});
+async function copier(folderPath, destPath) {
+  const folderCopy = await folder.mkdir(destPath, { recursive: true });
 
-  for(let file of files){
+  async function remove(somePath) {
+    const filesCopied = await folder.readdir(somePath, { withFileTypes: true });
+    for (let fileCopied of filesCopied) {
+
+      if (fileCopied.isDirectory()) {
+
+        let someNewPath = path.join(somePath, fileCopied.name);
+        await remove(someNewPath);
+        await folder.rm(someNewPath, { recursive: true });
+      } else if (fileCopied.isFile()) {
+        await folder.unlink(path.join(somePath, fileCopied.name));
+      }
+    }
+  }
+
+  await remove(destPath);
+
+  await folder.mkdir(destPath, { recursive: true });
+
+  const files = await folder.readdir(folderPath, { withFileTypes: true });
+
+  for (let file of files) {
     if (file.isFile()) {
       await folder.copyFile(path.join(folderPath, file.name), path.join(destPath, file.name));
 
     } else {
       const newPath = path.join(folderPath, file.name);
       const newDest = path.join(destPath, file.name);
-      copier (newPath, newDest);
+      copier(newPath, newDest);
     }
   };
 
-
 }
 
-bundler ();
-styler ();
+bundler();
+styler();
 const assetPath = path.join(__dirname, 'assets');
 const destAssetPath = path.join(__dirname, 'project-dist', 'assets');
 
-copier (assetPath, destAssetPath);
+copier(assetPath, destAssetPath);
